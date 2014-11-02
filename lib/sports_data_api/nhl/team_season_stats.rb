7849080c727dae796c2799bd1eb9c5a1954ab1e6
Alex Egan <aegan@andrew.cmu.edu>
2014-11-02 02:22:32 -0500

    refactored nhl team season stats
    Former-commit-id: f86d6acae74ece2e1c5a4be203aad0800db70fae

--- lib/sports_data_api/nhl/team_season_stats.rb
+++ lib/sports_data_api/nhl/team_season_stats.rb
@@ -8,49 +8,38 @@ module SportsDataApi
         @year = xml['year']
         @season = xml['type']
         @team = create_team(xml.xpath('team').first)
-        @opponents = create_opponents(xml.xpath('team/team_records/opponents').first)
+        @opponents = create_team_group(xml.xpath('team/team_records/opponents').first)
       end
 
+      # private helper methods
       private
-      def create_opponents(opponent_xml)
-        opponent_hash = Hash.new
-        stats_xml = opponent_xml.xpath('statistics').first
+      ##
+      # Creates the team hash for all team information
+      def create_team(team_xml)
+        team_hash = create_team_group(team_xml.xpath('team_records/overall').first)
+        team_xml.attributes.each { |k,v| team_hash[k.to_sym] = v.value }
+        team_hash
+      end
+
+      ##
+      # Creats a hash to map statistics for a team or opponent
+      def create_team_group(group_xml)
+        group_hash = Hash.new
+        group_hash[:statistics] = create_stats_group(group_xml.xpath('statistics').first)
+        group_hash[:goaltending] = create_stats_group(group_xml.xpath('goaltending').first)
+        group_hash[:shootouts] = map_attributes_to_hash(group_xml.xpath('shootout').first)
+        group_hash
+      end
+
+      ##
+      # Splits the stats xml into its nodes and creates a hash out of them
+      def create_stats_group(stats_xml)
         total_xml = stats_xml.xpath('total').first
         powerplay_xml = total_xml.xpath('powerplay').first
         shorthanded_xml = total_xml.xpath('shorthanded').first
         even_strength_xml = total_xml.xpath('evenstrength').first
         penalty_shots_xml = total_xml.xpath('penalty').first
-        opponent_hash[:statistics] = create_stats_hash(total_xml, powerplay_xml, shorthanded_xml, even_strength_xml, penalty_shots_xml)
-        goaltending_xml = opponent_xml.xpath('goaltending').first
-        total_xml = goaltending_xml.xpath('total').first
-        powerplay_xml = total_xml.xpath('powerplay').first
-        shorthanded_xml = total_xml.xpath('shorthanded').first
-        even_strength_xml = total_xml.xpath('evenstrength').first
-        penalty_shots_xml = total_xml.xpath('penalty').first
-        opponent_hash[:goaltending] = create_stats_hash(total_xml, powerplay_xml, shorthanded_xml, even_strength_xml, penalty_shots_xml)
-        opponent_hash[:shootouts] = map_attributes_to_hash(opponent_xml.xpath('shootout').first)
-        opponent_hash
-      end
-      ##
-      # Creates the team hash for all team information
-      def create_team(team_xml)
-        team_hash = Hash.new
-        team_xml.attributes.each { |k,v| team_hash[k.to_sym] = v.value }
-        total_xml = team_xml.xpath('team_records/overall/statistics/total').first
-        powerplay_xml = total_xml.xpath('powerplay').first
-        shorthanded_xml = total_xml.xpath('shorthanded').first
-        even_strength_xml = total_xml.xpath('evenstrength').first
-        penalty_shots_xml = total_xml.xpath('penalty').first
-        team_hash[:statistics] = create_stats_hash(total_xml, powerplay_xml, shorthanded_xml, even_strength_xml, penalty_shots_xml)
-        goaltending_xml = team_xml.xpath('team_records/overall/goaltending').first
-        total_xml = goaltending_xml.xpath('total').first
-        powerplay_xml = total_xml.xpath('powerplay').first
-        shorthanded_xml = total_xml.xpath('shorthanded').first
-        even_strength_xml = total_xml.xpath('evenstrength').first
-        penalty_shots_xml = total_xml.xpath('penalty').first
-        team_hash[:goaltending] = create_stats_hash(total_xml, powerplay_xml, shorthanded_xml, even_strength_xml, penalty_shots_xml)
-        team_hash[:shootouts] = map_attributes_to_hash(team_xml.xpath('team_records/overall/shootout').first)
-        team_hash
+        create_stats_hash(total_xml, powerplay_xml, shorthanded_xml, even_strength_xml, penalty_shots_xml)
       end
 
       ##
@@ -65,19 +54,22 @@ module SportsDataApi
         statistics_hash
       end
 
+      ##
+      # Maps the attributes and values of a node into a hash, converting
+      # the values into floats or integers
       def map_attributes_to_hash(attributes)
         attr_hash = Hash.new
         float_pattern = /([0-9]\.)+/
         attributes.each do |k, v|
-          if float_pattern.match("#{v}")
-            attr_hash[k.to_sym] = "#{v}".to_f
+          value_string = v.to_s
+          if float_pattern.match(value_string)
+            attr_hash[k.to_sym] = value_string.to_f
           else
-            attr_hash[k.to_sym] = "#{v}".to_i
+            attr_hash[k.to_sym] = value_string.to_i
           end
         end
         attr_hash
       end
-
 
     end
   end
