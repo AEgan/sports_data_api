41b491c2e2d10e0d961ab93c5f26d55094919717
Alex Egan <aegan@andrew.cmu.edu>
2014-11-02 01:24:30 -0400

    completes implementation and testing of total stats hash
    Former-commit-id: 2439d3c7d1d85a36fe57bb5113cdb8acbecf4c6b

--- lib/sports_data_api/nhl/team_season_stats.rb
+++ lib/sports_data_api/nhl/team_season_stats.rb
@@ -16,25 +16,41 @@ module SportsDataApi
       def create_team(team_xml)
         team_hash = Hash.new
         team_xml.attributes.each { |k,v| team_hash[k.to_sym] = v.value }
-        team_hash[:total] = create_total_stats(team_xml.xpath('team_records/overall/statistics/total').first)
+        total_xml = team_xml.xpath('team_records/overall/statistics/total').first
+        powerplay_xml = total_xml.xpath('powerplay').first
+        shorthanded_xml = total_xml.xpath('shorthanded').first
+        even_strength_xml = total_xml.xpath('evenstrength').first
+        penalty_shots_xml = total_xml.xpath('penalty').first
+        team_hash[:total] = create_total_stats(total_xml, powerplay_xml, shorthanded_xml, even_strength_xml, penalty_shots_xml)
         team_hash[:goaltending] = Hash.new
         team_hash
       end
 
       ##
       # Creates the hash for a team's total stats
-      def create_total_stats(total_xml)
-        total_hash = Hash.new
-        float_pattern = /([0-9]\.)+/
-        total_xml.attributes.each do |k, v|
-          if float_pattern.match(v.value)
-            total_hash[k.to_sym] = v.value.to_f
-          else
-            total_hash[k.to_sym] = v.value.to_i
-          end
-        end
+      def create_total_stats(total_xml, powerplay_xml, shorthanded_xml, even_strength_xml, penalty_shots_xml)
+        total_hash = map_attributes_to_hash(total_xml.attributes)
+        total_hash[:powerplay] = map_attributes_to_hash(powerplay_xml)
+        total_hash[:shorthanded] = map_attributes_to_hash(shorthanded_xml)
+        total_hash[:even_strength] = map_attributes_to_hash(even_strength_xml)
+        total_hash[:penalty_shots] = map_attributes_to_hash(penalty_shots_xml)
         total_hash
       end
+
+      def map_attributes_to_hash(attributes)
+        attr_hash = Hash.new
+        float_pattern = /([0-9]\.)+/
+        attributes.each do |k, v|
+          if float_pattern.match("#{v}")
+            attr_hash[k.to_sym] = "#{v}".to_f
+          else
+            attr_hash[k.to_sym] = "#{v}".to_i
+          end
+        end
+        attr_hash
+      end
+
+
     end
   end
 end
--- spec/lib/sports_data_api/nhl/team_season_stats_spec.rb
+++ spec/lib/sports_data_api/nhl/team_season_stats_spec.rb
@@ -11,6 +11,7 @@ describe SportsDataApi::Nhl::TeamSeasonStats, vcr: {
   end
   let(:team_stats) { SportsDataApi::Nhl.team_season_stats("44179d47-0f24-11e2-8525-18a905767e44", 2013, "REG") }
   let(:flyers) { team_stats.team }
+  let(:total) { flyers[:total] }
   context "team stats attributes" do
     subject { team_stats }
     describe 'meta methods' do
@@ -33,28 +34,70 @@ describe SportsDataApi::Nhl::TeamSeasonStats, vcr: {
       it { subject[:total].kind_of?(Hash).should be true  }
       it { subject[:goaltending].kind_of?(Hash).should be true  }
     end
-    describe "total information" do
-      it { subject[:total][:games_played].should eq 82 }
-      it { subject[:total][:goals].should eq 233 }
-      it { subject[:total][:assists].should eq 402 }
-      it { subject[:total][:penalties].should eq 429 }
-      it { subject[:total][:penalty_minutes].should eq 1180 }
-      it { subject[:total][:team_penalties].should eq 5 }
-      it { subject[:total][:team_penalty_minutes].should eq 10 }
-      it { subject[:total][:shots].should eq 2490 }
-      it { subject[:total][:blocked_att].should eq 1281 }
-      it { subject[:total][:missed_shots].should eq 1005 }
-      it { subject[:total][:hits].should eq 2174 }
-      it { subject[:total][:giveaways].should eq 587 }
-      it { subject[:total][:takeaways].should eq 445 }
-      it { subject[:total][:blocked_shots].should eq 1200 }
-      it { subject[:total][:faceoffs_won].should eq 2506 }
-      it { subject[:total][:faceoffs_lost].should eq 2507 }
-      it { subject[:total][:powerplays].should eq 274 }
-      it { subject[:total][:faceoffs].should eq 5013 }
-      it { subject[:total][:faceoff_win_pct].should eq 50.0 }
-      it { subject[:total][:shooting_pct].should eq 9.4 }
-      it { subject[:total][:points].should eq 635 }
+  end
+  context "total stats" do
+    subject { total }
+    describe "total" do
+      it { subject[:games_played].should eq 82 }
+      it { subject[:goals].should eq 233 }
+      it { subject[:assists].should eq 402 }
+      it { subject[:penalties].should eq 429 }
+      it { subject[:penalty_minutes].should eq 1180 }
+      it { subject[:team_penalties].should eq 5 }
+      it { subject[:team_penalty_minutes].should eq 10 }
+      it { subject[:shots].should eq 2490 }
+      it { subject[:blocked_att].should eq 1281 }
+      it { subject[:missed_shots].should eq 1005 }
+      it { subject[:hits].should eq 2174 }
+      it { subject[:giveaways].should eq 587 }
+      it { subject[:takeaways].should eq 445 }
+      it { subject[:blocked_shots].should eq 1200 }
+      it { subject[:faceoffs_won].should eq 2506 }
+      it { subject[:faceoffs_lost].should eq 2507 }
+      it { subject[:powerplays].should eq 274 }
+      it { subject[:faceoffs].should eq 5013 }
+      it { subject[:faceoff_win_pct].should eq 50.0 }
+      it { subject[:shooting_pct].should eq 9.4 }
+      it { subject[:points].should eq 635 }
+      it { subject[:powerplay].kind_of?(Hash).should be true }
+      it { subject[:shorthanded].kind_of?(Hash).should be true }
+      it { subject[:even_strength].kind_of?(Hash).should be true }
+      it { subject[:penalty_shots].kind_of?(Hash).should be true }
+      describe "powerplay" do
+        it { subject[:powerplay][:faceoffs_won].should eq 288 }
+        it { subject[:powerplay][:faceoffs_lost].should eq 260 }
+        it { subject[:powerplay][:shots].should eq 450 }
+        it { subject[:powerplay][:goals].should eq 58 }
+        it { subject[:powerplay][:missed_shots].should eq 187 }
+        it { subject[:powerplay][:assists].should eq 116 }
+        it { subject[:powerplay][:faceoff_win_pct].should eq 52.6 }
+        it { subject[:powerplay][:faceoffs].should eq 548 }
+      end
+      describe "shorthanded" do
+        it { subject[:shorthanded][:faceoffs_won].should eq 292 }
+        it { subject[:shorthanded][:faceoffs_lost].should eq 300 }
+        it { subject[:shorthanded][:shots].should eq 96 }
+        it { subject[:shorthanded][:goals].should eq 8 }
+        it { subject[:shorthanded][:missed_shots].should eq 25 }
+        it { subject[:shorthanded][:assists].should eq 7 }
+        it { subject[:shorthanded][:faceoff_win_pct].should eq 49.3 }
+        it { subject[:shorthanded][:faceoffs].should eq 592 }
+      end
+      describe "even_strength" do
+        it { subject[:even_strength][:faceoffs_won].should eq 1926 }
+        it { subject[:even_strength][:faceoffs_lost].should eq 1947 }
+        it { subject[:even_strength][:shots].should eq 1944 }
+        it { subject[:even_strength][:goals].should eq 167 }
+        it { subject[:even_strength][:missed_shots].should eq 793 }
+        it { subject[:even_strength][:assists].should eq 279 }
+        it { subject[:even_strength][:faceoff_win_pct].should eq 49.7 }
+        it { subject[:even_strength][:faceoffs].should eq 3873 }
+      end
+      describe "penalty_shots" do
+        it { subject[:penalty_shots][:shots].should eq 3 }
+        it { subject[:penalty_shots][:goals].should eq 0 }
+        it { subject[:penalty_shots][:missed_shots].should eq 0 }
+      end
     end
   end
 end
