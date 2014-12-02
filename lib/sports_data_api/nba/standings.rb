395fbd1eeee90405df06c89b7282c05418098d9e
Alex Egan <aegan@andrew.cmu.edu>
2014-12-01 22:41:08 -0500

    standings works down to teams

--- lib/sports_data_api/nba/standings.rb
+++ lib/sports_data_api/nba/standings.rb
@@ -22,8 +22,45 @@ module SportsDataApi
       private
       def create_conferences(conferences_xml)
         conferences_xml.each do |conference|
-          instance_variable_set("@#{conference['alias'].downcase}", {})
+          conference_hash = map_attributes_to_hash(conference.attributes)
+          conference_hash[:divisions] = create_divisions(conference.xpath('division'))
+          instance_variable_set("@#{conference['alias'].downcase}", conference_hash)
         end
+      end
+
+      # creates a hash for all divisions
+      def create_divisions(divisions_xml)
+        divisions_hash = Hash.new
+        divisions_xml.each do |division|
+          division_hash = map_attributes_to_hash(division.attributes)
+          division_hash[:teams] = create_teams(division.xpath('team'))
+          divisions_hash[division['name'].downcase.to_sym] = division_hash
+        end
+        divisions_hash
+      end
+
+      # creates the teams array
+      def create_teams(teams_xml)
+        teams_array = Array.new
+        teams_array
+      end
+
+      # maps attributes to a hash
+      def map_attributes_to_hash(attributes)
+        attr_hash = Hash.new
+        float_pattern = /([0-9]\.)+/
+        word_pattern = /([A-Za-z]|:)/
+        attributes.each do |k, v|
+          value_string = v.to_s
+          if float_pattern.match(value_string)
+            attr_hash[k.to_sym] = value_string.to_f
+          elsif word_pattern.match(value_string)
+            attr_hash[k.to_sym] = value_string
+          else
+            attr_hash[k.to_sym] = value_string.to_i
+          end
+        end
+        attr_hash
       end
 
     end
--- spec/lib/sports_data_api/nba/standings_spec.rb
+++ spec/lib/sports_data_api/nba/standings_spec.rb
@@ -10,6 +10,12 @@ describe SportsDataApi::Nba::Standings, vcr: {
     SportsDataApi.set_access_level(:nba, 't')
     SportsDataApi::Nba.standings(2013, :REG)
   end
+  let(:western) do
+    standings.western
+  end
+  let(:pacific) do
+    western[:divisions][:pacific]
+  end
   context "standings info" do
     subject { standings }
     it { should be_an_instance_of(SportsDataApi::Nba::Standings) }
@@ -17,5 +23,19 @@ describe SportsDataApi::Nba::Standings, vcr: {
     it { subject.season.should eq "REG" }
     it { subject.eastern.should be_an_instance_of(Hash) }
     it { subject.western.should be_an_instance_of(Hash) }
+  end
+  context "western info" do
+    subject { western }
+    it { subject.should be_an_instance_of(Hash) }
+    it { subject[:id].should eq "7fe7e212-de01-4f8f-a31d-b9f0a95731e3" }
+    it { subject[:name].should eq "WESTERN CONFERENCE" }
+    it { subject[:alias].should eq "WESTERN" }
+    it { subject[:divisions].should be_an_instance_of(Hash) }
+    it { subject[:divisions].length.should eq 3 }
+  end
+  context "pacific info" do
+    subject { pacific }
+    it { subject.should be_an_instance_of(Hash) }
+    it { subject[:teams].should be_an_instance_of(Array) }
   end
 end
